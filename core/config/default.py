956d62703514011509227f658d3bc2326f4aaaba
Yaroslav Khutornoy <morse1995@mail.ru>
2024-02-26 15:51:19 +0300

    Modify config

--- core/config/default.py
+++ core/config/default.py
@@ -8,9 +8,7 @@ _CFG = CN()
 _CFG.MODEL = CN()
 _CFG.MODEL.ARCHITECTURE = 'DCVC_HEM'
 _CFG.MODEL.DEVICE = "cpu"
-# _CFG.MODEL.MID_CHANNELS = 64
-# _CFG.MODEL.NUM_BLOCKS = 60
-# _CFG.MODEL.SPYNET_FREEZE_FOR_EPOCHS = 1
+_CFG.MODEL.PRETRAINED_WEIGHTS = ""
 
 # -----------------------------------------------------------------------------
 # INPUT
@@ -44,19 +42,19 @@ _CFG.DATA_LOADER.PIN_MEMORY = True
 # ---------------------------------------------------------------------------- #
 _CFG.SOLVER = CN()
 _CFG.SOLVER.LAMBDAS = [85, 170, 380, 840]
+_CFG.SOLVER.LR = 1e-4
 _CFG.SOLVER.STAGES = [
-    ['single', 'inter', 'single', 'me', 'none', '0.0001', '1'],
-    ['single', 'inter', 'single', 'me', 'me', '0.0001', '3'],
-    ['single', 'recon', 'single', 'rec', 'none', '0.0001', '3'],
-    ['single', 'recon', 'single', 'rec', 'rec', '0.0001', '3'],
-    ['single', 'all', 'single', 'rec', 'all', '0.0001', '6'],
-    ['dual', 'all', 'single', 'rec', 'all', '0.0001', '5'],
-    ['multi', 'all', 'single', 'rec', 'all', '0.0001', '3'],
-    ['multi', 'all', 'single', 'rec', 'all', '0.00001', '1'],
-    ['multi', 'all', 'cascade', 'rec', 'all', '0.00005', '2'],
-    ['multi', 'all', 'cascade', 'rec', 'all', '0.00001', '3']
+    ['1', 'inter', 'single', 'me', 'none', '0.0001', '1'],
+    ['1', 'inter', 'single', 'me', 'me', '0.0001', '3'],
+    ['1', 'recon', 'single', 'rec', 'none', '0.0001', '3'],
+    ['1', 'recon', 'single', 'rec', 'rec', '0.0001', '3'],
+    ['1', 'all', 'single', 'rec', 'all', '0.0001', '6'],
+    ['2', 'all', 'single', 'rec', 'all', '0.0001', '5'],
+    ['4', 'all', 'single', 'rec', 'all', '0.0001', '3'],
+    ['4', 'all', 'single', 'rec', 'all', '0.00001', '1'],
+    ['4', 'all', 'cascade', 'rec', 'all', '0.00005', '2'],
+    ['4', 'all', 'cascade', 'rec', 'all', '0.00001', '3']
   ]
-_CFG.SOLVER.MAX_EPOCH = 30
 
 # ---------------------------------------------------------------------------- #
 # Output options
